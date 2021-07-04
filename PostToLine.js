e09bdc2645f1a09722f6496e74ce15a354a80389
kitten1156 <frank1156kimo@gmail.com>
2021-07-05 00:50:18 +0800

    test change dns

--- PostToLine.js
+++ PostToLine.js
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.100.0/http/server.ts";
 const server = serve({ port: 3000 });
 for await (const request of server) {
 	if(request.headers.get("Authorization")){
-		//Deno.resolveDns("notify-api.line.me","A",{nameServer:{ ipAddr: "8.8.8.8", port: 53 }}).then(
+		Deno.resolveDns("notify-api.line.me","A",{nameServer:{ ipAddr: "8.8.8.8", port: 53 }}).then(
 			fetch(`https://notify-api.line.me/api/notify`, {
 				headers: {
 					"Authorization": request.headers.get("Authorization"),
@@ -12,7 +12,7 @@ for await (const request of server) {
 				body:request.url.substring(2)
 			}).then(response => response.text()).then(res => console.log(res))
 			.catch(error => console.error('Error:', error))
-		//)
+		)
 	} else {
 		console.log("No line key")
 	}
