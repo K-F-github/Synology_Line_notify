75ac422deee4b7f60a70698a4362ac3debc0ca3e
kitten1156 <frank1156kimo@gmail.com>
2021-07-04 23:10:32 +0800

    Update PostToLine.js
    add dns resolve

--- PostToLine.js
+++ PostToLine.js
@@ -2,7 +2,8 @@ import { serve } from "https://deno.land/std@0.100.0/http/server.ts";
 const server = serve({ port: 3000 });
 for await (const request of server) {
 	if(request.headers.get("Authorization")){
-		fetch('https://notify-api.line.me/api/notify', {
+		let ip = await Deno.resolveDns("notify-api.line.me","A",{nameServer:{ ipAddr: "8.8.8.8", port: 53 }}).then(res=> res[0])
+		fetch(`https://${ip}/api/notify`, {
 			headers: {
 				"Authorization": request.headers.get("Authorization"),
 				"Content-Type": "application/x-www-form-urlencoded"
